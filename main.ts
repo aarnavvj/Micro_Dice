25a50ae0a2b093ed7f6fa8b525af0fe669580c20
aarnavvj <81868372+aarnavvj@users.noreply.github.com>
2021-06-11 19:28:02 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,85 @@
+let Number2 = 0
+input.onButtonPressed(Button.A, function () {
+    Number2 = randint(1, 10)
+    if (Number2 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Number2 == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (Number2 == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . # . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (Number2 == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Number2 == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Number2 == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    } else if (Number2 == 7) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . # . #
+            . . . . .
+            # . . . #
+            `)
+    } else if (Number2 == 8) {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . # . #
+            `)
+    } else if (Number2 == 9) {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            # . # . #
+            . . . . .
+            # . # . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            # . . . #
+            . # . # .
+            # . . . #
+            `)
+    }
+})
